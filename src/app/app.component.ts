019742ec13245da188f63e219b94bb7e0f48f704
Yusa <yusa.oruc@stu.fsm.edu.tr>
2023-09-12 08:56:13 +0300

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Model } from './model';
+import { Model, ToDoItem} from './model';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +8,22 @@ import { Model } from './model';
 })
 export class AppComponent {
   model = new Model();
+  isDisplay = false;
 
   getName() {
     return this.model.user;
   }
 
   getItems() {
-    return this.model.items;
+    if(this.isDisplay){
+      return this.model.items;
+    }
+    return this.model.items.filter(item => !item.action);
+  }
+
+  addItem(value: any){
+    if (value!="") {
+        this.model.items.push(new ToDoItem(value,false));
+    }
   }
 }
